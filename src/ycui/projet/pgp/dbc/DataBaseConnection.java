20516d00db2069e1918a830708bfca23e2c36175
Yongzhi Cui <cuiyongzhi@hotmail.fr>
2016-02-01 03:33:10 +0100

    update

--- src/ycui/projet/pgp/dbc/DataBaseConnection.java
+++ src/ycui/projet/pgp/dbc/DataBaseConnection.java
@@ -8,7 +8,7 @@ public class DataBaseConnection {
 	private static final String DBDRIVER = "com.mysql.jdbc.Driver";
 	private static final String DBURL = "jdbc:mysql://localhost:3306/db_pgp";
 	private static final String USERNAME = "root";
-	private static final String PASSWORD = "";
+	private static final String PASSWORD = "123";
 	private Connection conn = null;
 	
 	public DataBaseConnection(){
