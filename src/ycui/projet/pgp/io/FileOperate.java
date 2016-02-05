cd71fad564b190ad5ca7d330e683c419e91e9ec6
Yongzhi Cui <cuiyongzhi@hotmail.fr>
2016-02-05 22:49:20 +0100

    update

--- src/ycui/projet/pgp/io/FileOperate.java
+++ src/ycui/projet/pgp/io/FileOperate.java
@@ -1,12 +1,6 @@
 package ycui.projet.pgp.io;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.util.TreeSet;
 
 import ycui.projet.pgp.exception.FileIOException;
--- src/ycui/projet/pgp/lang/Language.java
+++ src/ycui/projet/pgp/lang/Language.java
@@ -1,5 +1,6 @@
 package ycui.projet.pgp.lang;
 
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
