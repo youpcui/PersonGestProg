8240df6b557283138bd97a5348997225eecaa621
Yongzhi Cui <cuiyongzhi@hotmail.fr>
2016-02-10 00:51:33 +0100

    update

--- src/ycui/projet/pgp/exception/DAOException.java
+++ src/ycui/projet/pgp/exception/DAOException.java
@@ -5,6 +5,10 @@ package ycui.projet.pgp.exception;
  * DAOException
  */
 public class DAOException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	public DAOException(){}
 	public DAOException(String message){
 		super(message);
